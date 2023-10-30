6ab5f3152d906a199ca026457f26bc83fd73a265
nithinboddula <nithinboddula29@gmail.com>
2023-10-30 06:42:24 +0000

    solution

---
+++ src/components/Cart/index.js
@@ -0,0 +1,49 @@
+import Header from '../Header'
+import CartListView from '../CartListView'
+
+import CartContext from '../../context/CartContext'
+import CartSummary from '../CartSummary'
+import EmptyCartView from '../EmptyCartView'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList, removeAllCartItems} = value
+      const showEmptyView = cartList.length === 0
+      // TODO: Update the functionality to remove all the items in the cart
+      const onClickRemoveAllBtn = () => {
+        removeAllCartItems()
+      }
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+                <div className="remove-all-container">
+                  <button
+                    type="button"
+                    className="remove-all-btn"
+                    onClick={onClickRemoveAllBtn}
+                  >
+                    Remove All
+                  </button>
+                </div>
+                <CartListView />
+                {/* TODO: Add your code for Cart Summary here */}
+                <CartSummary />
+              </div>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+
+export default Cart
